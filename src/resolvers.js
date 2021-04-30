9923179abe437dea2c108be30fe6b8fcbbe04efe
Kenwanjohi <wanjohiken85@gmail.com>
2021-04-30 21:38:30 +0300

    tune node-postgres query

--- src/resolvers.js
+++ src/resolvers.js
@@ -13,8 +13,8 @@ const loaders = {
             genre: async (queries, {client}) => {     
             let genreids = queries.map(({ obj }) => obj.genreid)  
             let {rows} = await client.query(`
-            SELECT genreid, genredescription genre FROM genres WHERE  genres.genreid in (${genreids})
-            `)    
+            SELECT genreid, genredescription genre FROM genres WHERE  genres.genreid = ANY ($1)
+            `,[genreids])    
             return genreids.map(genreid => {
                return rows.filter(genreitem => genreitem.genreid === genreid)[0].genre
             })
